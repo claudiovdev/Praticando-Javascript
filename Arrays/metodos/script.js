1a9c266d0559fc3fbeabbbb9cd779bc02c5d67c7
viniciuscfn <claudio.vdev@gmail.com>
2021-07-04 19:11:41 -0300

    Finalizando os metodos

--- Arrays/metodos/script.js
+++ Arrays/metodos/script.js
@@ -1,5 +1,6 @@
-let item = ["faca", "peneira", "corda", "luva"];
-let mochila = [];
+let item = ["faca", "peneira", "corda", "luva","garfo", "alfinete", "vela", "alcool"];
+let numeros = [9,8,6,5,2,1,3,4,7];
+
 
 //Metodo push() adicionar o próximo item no final da array
 
@@ -19,3 +20,18 @@ item.shift()
 // Metodo splice() ele remove na posição que você indicar
 
 item.splice(1,1)
+
+// indexof serve para verificar se o está no array, ele busca e retorna a posição do array
+
+console.log(item.indexOf("garfo"))
+
+//Metodo sorte organizar os itens da array em ordem crescente
+//Metodo reverse ele reverte os numeros da array
+console.log(numeros.sort())
+console.log(numeros.reverse())
+
+
+// Metodo join traz os numeros separados por virgular
+
+numerosSeparados = numeros.join()
+console.log(numerosSeparados)
