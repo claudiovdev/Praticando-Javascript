d3c60864207a17ae1aab37b257fdbfe9a801b9bc
viniciuscfn <claudio.vdev@gmail.com>
2021-07-04 18:57:46 -0300

    trabalhando com os metodos da array

--- Arrays/metodos/script.js
+++ Arrays/metodos/script.js
@@ -1,2 +1,21 @@
-var iten = ["faca", "peneira", "corda", "luva"];
+let item = ["faca", "peneira", "corda", "luva"];
+let mochila = [];
 
+//Metodo push() adicionar o próximo item no final da array
+
+item.push("chave-Ingleas")
+
+//Metodo unshift adiciona no inicio do array
+item.unshift("fosforo")
+
+// Metodo Pop() remove o ultimo item do array
+
+item.pop()
+
+//Metodo shift ele remove o primeiro item da listas
+
+item.shift()
+
+// Metodo splice() ele remove na posição que você indicar
+
+item.splice(1,1)
